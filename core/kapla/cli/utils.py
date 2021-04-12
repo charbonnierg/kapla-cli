b9ee0968ef0e3d844d0491075837a485bc11f52c
Guillaume Charbonnier <Guillaume.Charbonnier@araymond.com>
2021-04-12 23:20:08 +0200

    feat(cli): introduced builder subcommand that can be used to manage docker images with builder.yml files

--- core/kapla/cli/utils.py
+++ core/kapla/cli/utils.py
@@ -5,7 +5,7 @@ import os
 import pathlib
 import subprocess
 import sys
-from typing import Iterator
+from typing import Dict, Iterable, Iterator
 
 
 @contextlib.contextmanager
@@ -39,3 +39,27 @@ def install_release_dependencies() -> None:
 
 def perform_release(debug: bool = True) -> None:
     run("semantic-release" + (" --debug" if debug else ""))
+
+
+def string_to_dict(string: str) -> Dict[str, str]:
+    key, value = string.split("=")
+    return {key: value}
+
+
+def map_string_to_dict(iterable: Iterable[str]) -> Dict[str, str]:
+    keyvalues = {}
+    if isinstance(iterable, (list, tuple, set)):
+        for string in iterable:
+            try:
+                keyvalues.update(string_to_dict(string))
+            except (TypeError, ValueError):
+                raise ValueError(f"Cannot parse string as dict: {string}")
+    elif isinstance(iterable, str):
+        for string in iterable.split(","):
+            try:
+                keyvalues.update(string_to_dict(string))
+            except (TypeError, ValueError):
+                raise ValueError(f"Cannot parse string as dict: {string}")
+    elif isinstance(iterable, dict):
+        keyvalues.update(iterable)
+    return keyvalues
